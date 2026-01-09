abcbdf111a3a1cc60b0725d717f15a617c0b47cc
euchxn <iyuchan90@gmail.com>
2026-01-09 14:57:14 +0900

    feat: clean init

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // 타입 에러 무시
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // 린트 에러 무시
+  },
 };
 
 export default nextConfig;
